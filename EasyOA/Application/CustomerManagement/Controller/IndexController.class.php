4903477959b0a99ee092fdb4333df4e2b8f02c99
JunChaoWeb <214502706@qq.com>
2016-07-22 01:46:30 +0800

    delete

--- EasyOA/Application/CustomerManagement/Controller/IndexController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace CustomerManagement\Controller;
-use Think\Controller;
-class IndexController extends Controller {
-    public function index(){
-        $this->show('CustomerManagement');
-    }
-}
